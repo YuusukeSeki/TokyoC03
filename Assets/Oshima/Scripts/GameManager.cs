f9d7461b07aea8c90754233bc753be528b79a9fd
oshimaharuna <ooooooshim115@gmail.com>
2018-09-13 21:55:15 +0900

    アイコンタップでspriteの入れ替え実装

--- Assets/Oshima/Scripts/GameManager.cs
+++ Assets/Oshima/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour {
 [SerializeField] AudioManager _audioManager = null;
 int gameState								= 0;
 
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,7 +19,17 @@ int gameState								= 0;
 	
 	// Update is called once per frame
 	void Update () {
-		//_uiManager.EditHpGauge(_player.maxHP, _player.hp);
+		_uiManager.EditHpGauge(_player.maxHp, _player.hp);
+
+		if(Input.GetMouseButtonDown(0)) {
+　　　		Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+　　　		Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+　　　		if (collition2d) {
+			// レイヤー名を取得
+    		string layerName = LayerMask.LayerToName(collition2d.transform.gameObject.layer);
+　　　　　		Debug.Log(layerName);
+　　　		}
+　		}
 	}
 
 	public void SceneChange(int id){
--- Assets/Oshima/Scripts/UIManager.cs
+++ Assets/Oshima/Scripts/UIManager.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 
 [SerializeField] Text[] texts 		= null;
-[SerializeField] Image[] images 	= null;//0:HPbar
+[SerializeField] Image[] Icons 	    = null;
+[SerializeField] Image[] Bars 	    = null;
 [SerializeField] Sprite[] sprites 	= null;
+//Image[] _spriteRenderer    = null;
+
+void Start(){
+    for(int i = 0; i<Icons.Length;i++){
+        //_spriteRenderer[i] = Icons[i].gameObject.GetComponent<SpriteRenderer>();
+    }
+}
 
 public void EditHpGauge(float maxHp, float nowHp){
     float hp = nowHp/maxHp;
-    images[0].fillAmount = hp;
+    Bars[0].fillAmount = hp;
 }
 
 public void EditMpGauge(){
@@ -24,6 +32,32 @@ public void EditItemNumber(){
 
 public void EditScore(){
 	
+}
+
+public void EditSprite(int num){
+    //Sprite touchSprite = _spriteRenderer[num].sprite;
+    //Sprite toSprite = _spriteRenderer[0].sprite;
+    Sprite touchSprite = Icons[num].sprite;
+    Sprite toSprite = Icons[0].sprite;
+
+    Icons[0].sprite = touchSprite;
+    Icons[num].sprite = toSprite;
+
+    Debug.Log("ok");
+}
+
+public void OnIconClick(int num){
+    //Debug.Log("icon"+num);
+    if(num == 0){
+        Debug.Log("skil");
+    }
+    else if(num == 3){
+        Debug.Log("friend");
+    }
+    else{
+        EditSprite(num);
+    }
+    
 }
 
 }
